942b862de51c3d2ba1b34d326b5be3e7ccbc01af
Prasetyo <prasetyosyifa@gmail.com>
2022-09-24 16:57:18 +0700

    slicing ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai"
+import Todo from "./components/Todo";
 
 function App() {
+  const [todos, setTodos] = useState(["Learn React", "war COC"])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">
+      <div className="container">
+        <h3 className="heading">Todo App</h3>
+        <form className="form">
+          <input className="input" type="text" placeholder="Add Todo" />
+          <button className="button">
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+        <p className="count">You have 2 todos</p>
+      </div>
     </div>
   );
 }
